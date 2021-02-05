e0eaad398f2ecddd63bb3dad27a2854756a07d80
Stefan Stark <starks@inf.ethz.ch>
2021-02-05 12:28:47 +0100

    Clean notebooks

--- pmvae/utils.py
+++ pmvae/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from scipy.linalg import block_diag
 from tensorflow.python.keras.layers import ops
@@ -121,3 +122,25 @@ class MaskedLayer(Dense):
             self.bias,
             self.activation,
             dtype=self._compute_dtype_object)
+
+
+def load_annotations(gmt, genes, min_genes=10):
+    genesets = parse_gmt(gmt, genes, min_genes)
+    annotations = pd.DataFrame(False, index=genes, columns=genesets.keys())
+    for key, genes in genesets.items():
+        annotations.loc[genes, key] = True
+
+    return annotations
+
+
+def parse_gmt(path, symbols=None, min_genes=10):
+    lut = dict()
+    for line in open(path, 'r'):
+        key, _, *genes = line.strip().split()
+        if symbols is not None:
+            genes = symbols.intersection(genes).tolist()
+        if len(genes) < min_genes:
+            continue
+        lut[key] = genes
+
+    return lut
